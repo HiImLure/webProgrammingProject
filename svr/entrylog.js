b521b6cf93abb76954a7d7a287c41cffd7a6130f
HiimLure <ahmad.alkaabi5@outlook.com>
2023-05-12 16:21:04 +0100

    changes

--- svr/entrylog.js
+++ svr/entrylog.js
@@ -2,6 +2,7 @@ import uuid from 'uuid-random';
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 
+// Initialises a connection to the SQLite database with the filename './logDatabase', which are then migrated to the ./sql directory 
 async function init() {
   const database = await open({
     filename: './logDatabase', 
@@ -15,24 +16,29 @@ async function init() {
 
 const connection = init();
 
+
 export async function logList() {
   const database = await connection;
-  return await database.all('SELECT * FROM logs ORDER BY ROWID ASC LIMIT 30');  
+  return await database.all('SELECT * FROM logs ORDER BY date DESC LIMIT 30');  //Returns the list of logs with a limit of 30 items, ordered by date in descending order.
 }
 
 export async function findLog(id) {
   const database = await connection;
-  return database.get('SELECT * FROM logs WHERE id = ?', id); 
+  return database.get('SELECT * FROM logs WHERE id = ?', id); // Finds and returns the log with the selected id.
 }
 
-const options = {year: '2-digit', month: 'numeric', day: 'numeric'};
-const today  = new Date();
+function currentTime() {
+  const isoString = new Date().toISOString();
+  const formattedTime = isoString.slice(2, -5).replace('T', ' ');
+  return formattedTime;
+}
 
+//Creates a new log with the given work, exp, and comp values. The id and date fields are automatically given. Returns the updated list of logs after they are inserted in the database.
 export async function newLog(work, exp, comp) {
   const database = await connection;
 
   const id = uuid();
-  const date = today.toLocaleDateString("en-gb", options);
+  const date = currentTime();
   await database.run('INSERT INTO logs VALUES (?,?,?,?,?)',[id, date, work, exp,comp ]);
 
   return logList();
--- svr/server.js
+++ svr/server.js
@@ -1,4 +1,3 @@
-
 import * as lg from './entrylog.js';
 import express from 'express';
 
@@ -6,20 +5,25 @@ const app = express();
 
 app.use(express.static('client', { extensions: ['html'] }));
 
+
+// retrives a list of logs using GET method
 async function getLogs(req, res){
   res.json(await lg.logList());
 }
 
+// retrives a single log through a specific ID 
 async function getLog(req, res){
   const result = lg.findLog(req.params.id);
   if (result) { res.json(result); } else { res.status(404).send('No match');}
 }
 
+// creates a new log parsing json body of work, exp, and comp parameters
 async function postLogs(req,res){
   const msg = await lg.newLog(req.body.work, req.body.exp, req.body.comp);
   res.json(msg);
 }
 
+// referenced from simple-staged-messageboard
 function asyncWrap(f) {
   return (req,res,next) => {
     Promise.resolve(f(req, res, next))
@@ -30,7 +34,7 @@ function asyncWrap(f) {
 
 
 
-app.get('/logs', asyncWrap(getLogs));
+app.get('/logs', asyncWrap(getLogs)); 
 app.get('/logs/:id', asyncWrap(getLog));
 app.post('/logs', express.json(), asyncWrap(postLogs));
 
